54425566509a81e88bf57c764712f019be5d6716
Adam Jones <domdomegg+git@gmail.com>
2023-04-22 20:32:21 +0100

    Add version number to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>BlueBot control panel</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="version" content={process.env.VERSION} />
       </Head>
       <Component {...pageProps} />
     </>
